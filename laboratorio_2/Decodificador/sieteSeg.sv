6df4b7c4b84a80dcfdea7d9bfd5a070f04827a42
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2023-08-23 01:14:32 -0600

    Cambios en el Sumador

--- laboratorio_2/Decodificador/sieteSeg.sv
+++ laboratorio_2/Decodificador/sieteSeg.sv
@@ -1,18 +1,20 @@
-module sieteSeg(input logic[3:0] data, output logic [6:0] segments);
-	always_comb 
-		case(data)
-		
-		0: segments=7'b111_1110;
-		1: segments=7'b011_0000;
-		2: segments=7'b110_1101;
-		3: segments=7'b111_1001;
-		4: segments=7'b011_0011;
-		5: segments=7'b101_1011;
-		6: segments=7'b101_1111;
-		7: segments=7'b111_0000;
-		8: segments=7'b111_1111;
-		9: segments=7'b111_0011;
-		default: segments = 7'b000_0000;
-		
-	endcase
+module sieteSeg(input wire [3:0]x, output reg [6:0]salida);
+
+	always@(*)
+	begin
+		case(x)
+			0: salida = 7'b0000001;
+			1: salida = 7'b1001111;
+			2: salida = 7'b0010010;
+			3: salida = 7'b0000110;
+			4: salida = 7'b1001100;
+			5: salida = 7'b0100100;
+			6: salida = 7'b0100000;
+			7: salida = 7'b0001111;
+			8: salida = 7'b0000000;
+			9: salida = 7'b0000100;
+			default: salida = 7'b0000001;
+		endcase
+	end
+
 endmodule
