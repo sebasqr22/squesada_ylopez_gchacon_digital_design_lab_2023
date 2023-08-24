7c8b611b613ecb698651dc10f3bd0a5e944954c0
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2023-08-23 19:37:02 -0600

    Contador

--- laboratorio_2/Contador/contador_tb.sv
+++ laboratorio_2/Contador/contador_tb.sv
@@ -1,13 +1,10 @@
 module contador_tb;
-
-    // Señales de entrada y salida del test bench
     logic clock;
     logic reset;
     logic [1:0] carga;
     logic [1:0] cuenta;
 
-    // Instancia del contador bajo prueba
-    contador #(2) dut (
+    contador #(4) dut (
         .clock(clock),
         .reset(reset),
         .carga(carga),
@@ -15,18 +12,16 @@ module contador_tb;
     );
 
     initial begin
-        // Simulación para 2 bits
-        $display("Testing for 2 bits...");
+        $display("Testing for 4 bits...");
         clock = 0;
         reset = 1;
-        carga = 2'b11; // Carga inicial
+        carga = 4'b11;
         #5 reset = 0;
-
-        // Ciclos de simulación
-        #15; // Cambiar este valor si es necesario
-
-        $display("Carga: %b, Cuenta: %b", carga, cuenta);
-        $finish;
+        #15;
+        $display("Se esta cargando: %b, Esta contando: %b", carga, cuenta);
     end
+	 
+	 
+
 
 endmodule
