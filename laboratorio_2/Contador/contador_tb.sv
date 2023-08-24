e0562201c9f9aefb47e8e469d546bdef9e7eff18
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2023-08-23 22:49:01 -0600

    Arreglo del contador

--- laboratorio_2/Contador/contador_tb.sv
+++ laboratorio_2/Contador/contador_tb.sv
@@ -1,27 +1,66 @@
-module contador_tb;
-    logic clock;
-    logic reset;
-    logic [1:0] carga;
-    logic [1:0] cuenta;
+module contador_tb();
 
-    contador #(4) dut (
-        .clock(clock),
-        .reset(reset),
-        .carga(carga),
-        .cuenta(cuenta)
-    );
-
-    initial begin
-        $display("Testing for 4 bits...");
-        clock = 0;
-        reset = 1;
-        carga = 4'b11;
-        #5 reset = 0;
-        #15;
-        $display("Se esta cargando: %b, Esta contando: %b", carga, cuenta);
-    end
-	 
-	 
-
+	logic clock,reset;
+	logic [5:0] cuenta;
+	logic [5:0] carga;
+	logic [3:0] cuenta2;
+	logic [3:0] carga2;
+	logic [1:0] carga3;
+	logic [1:0] cuenta3;
+	
+	
+	contador #(.N(6)) cont1(clock, reset,carga, cuenta);
+	
+	
+	contador #(.N(4)) cont2(clock, reset,carga2, cuenta2);
+	
+	
+	contador #(.N(2)) cont3(clock, reset,cuenta3, carga3);
+	
+	
+	
+	
+	initial begin
+	
+		carga = 6'b111111;
+		carga2 = 4'b1111;
+		cuenta3 = 2'b11;
+		reset = 0; #10;
+		reset = 1; #10;
+		clock = 0; #10;
+	
+		assert (carga == 6'b111111)else $error("fallo prueba 1");
+		
+		assert (carga2 == 4'b1111)else $error("fallo prueba 2");
+		
+		assert (cuenta3 == 2'b11)else $error("fallo prueba 3");
+		
+		
+		
+		clock = 1; #10;
+		clock = 0; #10;
+		
+		
+		assert(cuenta == carga-2'b10) else $error("error");
+		$display("p1 correcto");
+		
+		assert(cuenta2 == carga2-2'b10) else $error("error");
+		$display("p2 correcto");
+		
+		assert(carga3== cuenta3-2'b10) else $error("error");
+		$display("p3 correcto");
+		
+		
+	end
+	
+	
+	always begin
+	
+		clock = !clock; #10;
+		
+	
+	end
+	
+	
 
 endmodule
