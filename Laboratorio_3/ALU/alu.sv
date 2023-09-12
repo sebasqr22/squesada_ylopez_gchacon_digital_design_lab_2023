401cb255a01b1c6172dfb7f52423583c8fece20e
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2023-09-12 02:20:29 -0600

    Cambios
    Co-Authored-By: Yaritza E Lopez B <48070919+yaritzaelena@users.noreply.github.com>
    Co-Authored-By: GChacon8 <85459837+GChacon8@users.noreply.github.com>

--- Laboratorio_3/ALU/alu.sv
+++ Laboratorio_3/ALU/alu.sv
@@ -1,15 +1,18 @@
 module alu #(parameter N = 4)(input logic [3:0] A,B, input wire [3:0] sel, output logic OF, output logic carry_, output logic cero, output logic neg, output logic [3:0] result);
 
 
-wire [N-1:0] andR, orR, xorR, multiR, sumaR, shiftLR, shiftRR;
+wire [N-1:0] andR, orR, xorR, multiR, sumaR, shiftLR, shiftRR, restaR, divR;
 
 CompAnd #(N)  inst_and (.A(A), .B(B), .R(andR));
 CompXor#(N)  inst_xor (.A(A), .B(B), .R(xorR));
 CompOr#(N)  inst_or (.A(A), .B(B), .R(orR));
-//Mult#(N)  inst_or (.A(A), .B(B), .R(multiR));
+mult#(N)  inst_mult (.A(A), .B(B), .Producto(multiR), .Overflow(OF));
 NBitAdder#(N)  inst_nBitAdder (.A(A), .B(B), .Sum(sumaR), .Overflow(OF));
 ShiftL#(N)  inst_sl (.A(A), .shift(B), .R(shiftLR));
 ShiftR#(N)  inst_sr (.A(A), .shift(B), .R(shiftRR));
+//resta#(N)  inst_resta (.A(A), .B(B), .Neg(neg) , .R(restaR));
+
+div#(N)  inst_div (.Dividendo(A), .Divisor(B), .Cociente(divR), .Residuo(cero), .DivisionByZeroError(neg));
 
 
 logic [N-1:0] tmp;
@@ -29,14 +32,18 @@ logic [N-1:0] tmp;
 			4'b0011: //SUMA
 				tmp = sumaR;
 				
-			4'b0100: //SHIFTL
+			4'b0100: //SHIFT L
 				tmp = shiftLR;
 				
-			4'b0101:
+			4'b0101:	//SHIFT R
 				tmp = shiftRR;
-				/*
-			4'b0110:
-			4'b0111: 
+				
+			4'b0110: //MULT
+				tmp = multiR;
+				
+			4'b0111: // DIV
+				tmp = divR;
+			/*
 			4'b1000:
 			4'b1001:*/
 			default: tmp = andR;
